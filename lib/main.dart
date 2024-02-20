4ce8b228eea4467f61bc94febae400a5abee97a2
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-20 21:36:07 +0100

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,53 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isFavorited = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "My App"
+    return MaterialApp(
+      title: "My App",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.blue,
+            leading: IconButton(
+              icon: Icon(
+                _isFavorited ? Icons.favorite : Icons.favorite_border,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isFavorited = !_isFavorited;
+                });
+              },
+            ),
+            title: const Text("My App",
+                style: TextStyle(color: Colors.white, fontSize: 20)
+            )
+        ),
+        body: const Center(
+          child: Column(
+            children: [
+              Text("DEV101",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold)),
+              Text("DEVOAM201",
+                  style: TextStyle(color: Colors.blue, fontSize: 30))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
