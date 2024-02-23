2f8374cfa18abf8924061eb5a15ec2a2fac82ea0
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-23 14:51:32 +0100

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,103 @@
 import 'package:flutter/material.dart';
+class Task {
+  String title;
+  bool isDone;
+
+  Task({required this.title, this.isDone = false});
+}
 
 void main() {
-  runApp(const FirstApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaskTracker(),
+    );
+  }
+}
+
+class TaskTracker extends StatefulWidget {
+  @override
+  _TaskTrackerState createState() => _TaskTrackerState();
+}
+
+class _TaskTrackerState extends State<TaskTracker> {
+  List<Task> tasks = [];
+
+  void _addTask(String title) {
+    setState(() {
+      tasks.add(Task(title: title));
+    });
+  }
+
+  void _toggleDone(int index) {
+    setState(() {
+      tasks[index].isDone = !tasks[index].isDone;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Task Tracker'),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              tasks[index].title,
+              style: TextStyle(
+                decoration: tasks[index].isDone ? TextDecoration.lineThrough : null,
+              ),
+            ),
+            trailing: Checkbox(
+              value: tasks[index].isDone,
+              onChanged: (bool? value) {
+                _toggleDone(index);
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddTaskDialog();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _showAddTaskDialog() {
+    TextEditingController controller = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add New Task'),
+          content: TextField(
+            controller: controller,
+            decoration: InputDecoration(hintText: "Enter task name"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                if (controller.text.isNotEmpty) {
+                  _addTask(controller.text);
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
