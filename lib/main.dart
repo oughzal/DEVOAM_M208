42a3430836e4ad5adb0b7f63a7e23c93407a197d
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-29 01:29:33 +0100

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,23 @@ void main() {
   runApp(const FirstApp());
 }
 
+class FirstApp extends StatefulWidget {
+  const FirstApp({super.key});
+
+  @override
+  State<FirstApp> createState() => _FirstAppState();
+}
+
+class _FirstAppState extends State<FirstApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "Title",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First App"),
+        ),
+      ),
+    );
+  }
+}
