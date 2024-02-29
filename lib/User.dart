62037ad35870f68488b81943c003641c3d3cd3d7
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-29 01:56:58 +0100

    Intial Commit

---
+++ lib/User.dart
@@ -0,0 +1,15 @@
+class User {
+  final int id;
+  final String firstName;
+  final String lastName;
+
+  User({required this.id, required this.firstName, required this.lastName});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'User.dart';
 
 void main() {
   runApp(const FirstApp());
@@ -12,13 +16,44 @@ class FirstApp extends StatefulWidget {
 }
 
 class _FirstAppState extends State<FirstApp> {
+  Future<List<User>> fetchUsers() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2/restAPI/userApi.php'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> userJson = json.decode(response.body);
+      return userJson.map((json) => User.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      title: "Title",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("First App"),
+          title: Text('Liste des Utilisateurs'),
+        ),
+        body: FutureBuilder<List<User>>(
+          future: fetchUsers(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Erreur: ${snapshot.error}"));
+            } else {
+              // Les données sont chargées, afficher la liste des utilisateurs
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  User user = snapshot.data![index];
+                  return ListTile(
+                    title: Text('${user.firstName} ${user.lastName}'),
+                    subtitle: Text('ID: ${user.id}'),
+                  );
+                },
+              );
+            }
+          },
         ),
       ),
     );
