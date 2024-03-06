f0eec9af5b8e4c81e32adf7898a6beee54513a91
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-03-06 14:08:01 +0100

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,16 @@ class FirstApp extends StatefulWidget {
 }
 
 class _FirstAppState extends State<FirstApp> {
+  void _onButtonPress(){
+    switch(_currentIndex){
+      case 0 : _selected = "Home";break;
+      case 1 : _selected = "Edit";break;
+      case 2 : _selected = "Settings";break;
+    }
+    setState(() {
+
+    });
+  }
   int _currentIndex = 0;
   List<String> items = ["Home", "Edite", "Setting"];
   String _selected = "Home";
@@ -19,37 +29,39 @@ class _FirstAppState extends State<FirstApp> {
     String item = items[index];
 
     setState(() {
-        _currentIndex = index;
-        _selected=items[index];
+        _selected=item;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Title",
+      title: "My App",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Bottom Navigation"),
         ),
-        body: Center(
-          child: Text("Selected item : $_selected",
+        body: Column(
+          children: [Text("Selected item : $_selected",
             style: TextStyle(fontSize: 30,
               fontWeight: FontWeight.bold
             ),
           ),
+            ElevatedButton(onPressed: _onButtonPress, child: Text("Edite"))
+        ]
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
             //const BottomNavigationBarItem(icon: const Icon(Icons.login), label: "Login"),
             BottomNavigationBarItem(icon: Icon(Icons.edit), label: "edit"),
+
             BottomNavigationBarItem(
                 icon: Icon(Icons.settings), label: "Settings"),
           ],
           currentIndex: _currentIndex,
           onTap: _OnTabBottomItem,
-          selectedItemColor: Colors.blue,
+          selectedItemColor: Colors.red,
         ),
       ),
     );
