e3ea6d57a0d4e618f84cf671edba13799117757e
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-03-06 01:09:23 +0100

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,44 @@ class FirstApp extends StatefulWidget {
 }
 
 class _FirstAppState extends State<FirstApp> {
+  int _currentIndex = 0;
+  List<String> items = ["Home", "Edite", "Setting"];
+  String _selected = "Home";
+  void _OnTabBottomItem(int index) {
+    String item = items[index];
+
+    setState(() {
+        _currentIndex = index;
+        _selected=items[index];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: "Title",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("First App"),
+          title: Text("Bottom Navigation"),
+        ),
+        body: Center(
+          child: Text("Selected item : $_selected",
+            style: TextStyle(fontSize: 30,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            //const BottomNavigationBarItem(icon: const Icon(Icons.login), label: "Login"),
+            BottomNavigationBarItem(icon: Icon(Icons.edit), label: "edit"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings"),
+          ],
+          currentIndex: _currentIndex,
+          onTap: _OnTabBottomItem,
+          selectedItemColor: Colors.blue,
         ),
       ),
     );
