de0fbccba7628d493f1e6f8ad85576fab62c62f2
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-03-19 14:11:05 +0000

    Intial Commit

---
+++ lib/User.dart
@@ -0,0 +1,7 @@
+class User{
+
+  final String nom;
+  final String prenom;
+  User({required this.nom, required this.prenom});
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'User.dart';
 
 void main() {
   runApp(const FirstApp());
@@ -12,10 +15,17 @@ class FirstApp extends StatefulWidget {
 }
 
 class _FirstAppState extends State<FirstApp> {
+  List<User> users=[];
+
+  int _position = -1;
+
+  TextEditingController nomController = TextEditingController() ;
+  TextEditingController prenomController = TextEditingController() ;
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
+
       title: "Title",
       home: Scaffold(
         appBar: AppBar(
@@ -23,6 +33,73 @@ class _FirstAppState extends State<FirstApp> {
           leading: const Icon(Icons.home , color: Colors.white,),
           title: Text("My App", style: TextStyle(color: Colors.white),),
         ),
+        body: Column(
+          children: [
+            TextField(controller: nomController,
+            decoration: InputDecoration(
+              labelText: "Le nom" ,
+            ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(controller: prenomController,
+              decoration: InputDecoration(
+                labelText: "Le prenom" ,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            ElevatedButton(onPressed: (){
+              User user= User(nom : nomController.text.toString(),prenom: prenomController.text.toString());
+              if(_position == -1 ){
+                setState(() {
+                  users.add(user);
+                });
+              }else{
+                setState(() {
+                  users[_position]= user;
+                });
+
+              }
+              _position = -1;
+
+            }, child: const Text("Save"),),
+            SizedBox(
+              height: 10,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: users.length,
+                  itemBuilder: (context,position){
+                    User user = users[position];
+                    return Row(
+                      children: [
+                        Text("${user.nom} ${user.prenom}"),
+                        ElevatedButton(onPressed: (){
+                          _position = position;
+                              setState(() {
+                            nomController.text = users[position].nom;
+                            prenomController.text = users[position].prenom;
+
+                          });
+
+                        }, child: Icon(Icons.edit)),
+                        ElevatedButton(onPressed: (){
+                          setState(() {
+                            users.removeAt(position);
+                          });
+                        }, child: Icon(Icons.delete)),
+                      ],
+                    );
+                  }
+              ),
+            )
+          ],
+        ) ,
       ),
     );
   }
