5742705f8ed9f80e1f15592ff2cf4b3eb136389e
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-03-25 23:45:36 +0000

    Commit

--- lib/User.dart
+++ lib/User.dart
@@ -1,10 +1,20 @@
 class User{
-  int id;
-  String firstName;
-  String lastName;
-
-
+  final int? id;
+  late final String firstName;
+  late final String lastName;
   User({required this.id,required this.firstName,required this.lastName});
-
-  toMap() {}
+  factory User.fromMap(Map<String, dynamic> data) {
+    return User(
+      id: data['id'],
+      firstName: data['firstName'],
+      lastName: data['lastName'],
+    );
+  }
+  Map<String,dynamic> toMap(){
+    return {
+      "id" : id,
+      "firstName" : firstName,
+      "lastName" :lastName,
+    };
+  }
 }
